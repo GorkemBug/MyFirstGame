76d507fe43c60f43e9ad99a10d341e85047604bc
GorkemBug <bugur.gorkem@gmail.com>
2020-10-28 14:23:53 +0300

    Autoplay code changes

--- Assets/Scripts/AutoPlay.cs
+++ Assets/Scripts/AutoPlay.cs
@@ -6,20 +6,12 @@ using UnityEngine;
 
 public class AutoPlay : MonoBehaviour
 {
-    [SerializeField] bool autoPlay;
+    [SerializeField] Boolean autoPlay;
     [SerializeField] private TextMeshProUGUI AutoPlayText;
     public void AutoPlayOnOff()
     {
-        if (autoPlay == false )
-        {
-            autoPlay = true;
-            AutoPlayText.text = "On";
-        }
-        else if(autoPlay == true )
-        {
-            autoPlay = false;
-            AutoPlayText.text = "Off";
-        }
+        autoPlay = !autoPlay;
+        AutoPlayText.text = autoPlay.Equals(true) ? "On" : "Off";
     }
 
     private void Update()
