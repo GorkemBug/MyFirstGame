90522ba94b17be9f75ce3f0a406f6096eb75550c
GorkemBug <bugur.gorkem@gmail.com>
2020-10-28 14:01:07 +0300

    Added Auto play & some quality changes

---
+++ Assets/Scripts/AutoPlay.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class AutoPlay : MonoBehaviour
+{
+    [SerializeField] bool autoPlay;
+    [SerializeField] private TextMeshProUGUI AutoPlayText;
+    public void AutoPlayOnOff()
+    {
+        if (autoPlay == false )
+        {
+            autoPlay = true;
+            AutoPlayText.text = "On";
+        }
+        else if(autoPlay == true )
+        {
+            autoPlay = false;
+            AutoPlayText.text = "Off";
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            AutoPlayOnOff();
+        }
+    }
+
+    public bool IsAutoPlayEnabled()
+    {
+        return autoPlay;
+    }
+}
